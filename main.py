fec30f0309ded99bcd5292b1fe4a004ee5a5e67f
Nitin Gaikwad <nitin.m.gaikwad@gmail.com>
2024-08-16 20:25:44 +0530

    Add config  file
    Addition of config file

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from config import settings
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"message": "Hello World"}
+
+@app.get("/info")
+async def info():
+    return {
+        "app name":settings.app_name
+    }
